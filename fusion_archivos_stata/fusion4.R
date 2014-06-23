8c09917abafbc95adf0c6e0bd8adeb5f56447347
joscani <canadasreche@gmail.com>
2014-06-23 13:39:30 +0200

    Scripts para fusionar módulos de share y crear un archivo .RData para cada ola

---
+++ fusion_archivos_stata/fusion4.R
@@ -0,0 +1,166 @@
+##################################
+# Envejecimiento activo
+# Creación fichero desde los datos originales
+# Aut: José Luis Cañadas
+# Fecha: Junio 2014 (para reproducibilidad)
+##################################
+
+
+library(foreign)
+
+# sigo esquema del documento, mas tarde agruparé como en 3_2.R
+# variables actividad vida diaria
+
+# adl: En fichero (physical healt) de variables  generadas
+
+## Módulo AC 
+ac <- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_ac.dta")
+names(ac)
+sel.id <- c("mergeid","country")
+sel1 <- paste0("ac035d",c(1,4,5:10))
+sel2 <- paste0("ac036_",c(1,4:10))
+
+seleccion <- c(sel.id,sel1,"ac035dno",sel2)
+
+posicion <- names(ac) %in%seleccion
+ac.new <- ac[,posicion]
+names(ac.new)
+
+
+
+## Módulo IV
+iv <- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_iv.dta")
+
+
+posicion <- names(iv)%in%c("mergeid","iv020_")
+iv.new <- iv[,posicion]
+names(iv.new)
+
+
+
+unido1 <- merge(ac.new,iv.new,by="mergeid")
+
+
+## Módulo DN
+
+dn <- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_dn.dta")
+
+posicion <- names(dn)%in%c("mergeid","dn003_","dn010_","dn014_","gender")
+dn.new <- dn[,posicion]
+
+
+
+# uno unido1 con dn.new por la variable "mergeid"
+unido2 <- merge(unido1, dn.new, by="mergeid")
+
+
+# borro unido1
+rm(unido1)
+
+## Módulo PH
+
+ph <- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_ph.dta")
+
+sel4 <- paste0("ph006d",c(1,4,5,6,10,12,14))
+
+posicion <- names(ph)%in%c("mergeid",sel4)
+
+ph.new <- ph[,posicion]
+
+
+
+# uno unido2 con ph.new por la variable "mergeid"
+unido3 <- merge(unido2, ph.new, by="mergeid")
+
+
+
+# borro unido2
+rm(unido2)
+
+
+## variables generadas gen_ph
+
+  gen_ph <- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_generated_modules/sharew4_rel1-1-1_gv_health.dta")
+
+posicion <- names(gen_ph)%in%c("mergeid","adl", "iadl","numeracy","eurod")
+gen_ph.new <- gen_ph[,posicion]
+
+
+
+# uno unido3 con gen_ph.new por la variable "mergeid"
+unido4 <- merge(unido3, gen_ph.new, by="mergeid")
+
+
+
+# borro unido3
+rm(unido3)
+
+# Módulo CF
+
+cf<- read.dta("../an_longitudinal/data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_cf.dta")
+
+posicion <- names(cf)%in%c("mergeid","cf003_","cf004_","cf005_","cf006_",
+                           "cf012_","cf013_","cf014_","cf015_")
+cf.new <- cf[,posicion]
+
+# pongo los atributos correspondientes a las variables seleccionadas
+
+attributes(cf.new)$val.labels <- attributes(cf)$val.labels[posicion]
+attributes(cf.new)$var.labels <- attributes(cf)$var.labels[posicion]
+
+# uno unido4 con cf.new por la variable "mergeid"
+unido5 <- merge(unido4, cf.new, by="mergeid")
+
+# creo los atributos de unido4
+attributes(unido5)$val.labels <- c(attributes(unido4)$val.labels,attributes(cf.new)$val.labels[-1])
+attributes(unido5)$var.labels <- c(attributes(unido4)$var.labels,attributes(cf.new)$var.labels[-1])
+
+# borro unido4
+rm(unido4)
+
+# Módulo MH
+
+mh<- read.dta("../data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_mh.dta")
+posicion <- names(mh)%in%c("mergeid",paste0(paste0("mh00",2:9),"_"),
+                          paste0(paste0("mh0",10:17),"_"))
+mh.new <- mh[,posicion]
+
+# pongo los atributos correspondientes a las variables seleccionadas
+
+attributes(mh.new)$val.labels <- attributes(mh)$val.labels[posicion]
+attributes(mh.new)$var.labels <- attributes(mh)$var.labels[posicion]
+
+# uno unido5 con mh.new por la variable "mergeid"
+unido6 <- merge(unido5, mh.new, by="mergeid")
+
+# creo los atributos de unido5
+attributes(unido6)$val.labels <- c(attributes(unido5)$val.labels,attributes(mh.new)$val.labels[-1])
+attributes(unido6)$var.labels <- c(attributes(unido5)$var.labels,attributes(mh.new)$var.labels[-1])
+
+# borro unido5
+rm(unido5)
+
+# Módulo EP
+
+ep<- read.dta("../data/wave4/stata_sharew4_rel1-1-1_all_capi_modules/sharew4_rel1-1-1_ep.dta")
+posicion <- names(ep) %in% c("mergeid", "ep005_")
+ep.new <- ep[,posicion]
+
+# pongo los atributos correspondientes a las variables seleccionadas
+
+attributes(ep.new)$val.labels <- attributes(ep)$val.labels[posicion]
+attributes(ep.new)$var.labels <- attributes(ep)$var.labels[posicion]
+
+# uno unido6 con ep.new por la variable "mergeid"
+unido7 <- merge(unido6, ep.new, by="mergeid")
+
+# creo los atributos de unido6
+attributes(unido7)$val.labels <- c(attributes(unido6)$val.labels,attributes(ep.new)$val.labels[-1])
+attributes(unido7)$var.labels <- c(attributes(unido6)$var.labels,attributes(ep.new)$var.labels[-1])
+
+# borro unido6
+rm(unido6)
+
+ola4 <- unido7
+
+
