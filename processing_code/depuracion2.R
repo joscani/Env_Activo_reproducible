eb854ec1ddd2d1c7edd34d36874b718f8b45b69d
joscani <canadasreche@gmail.com>
2014-06-25 11:01:35 +0200

    se comenta linea de salvar los datos

--- processing_code/depuracion2.R
+++ processing_code/depuracion2.R
@@ -73,4 +73,4 @@ rm(paises.no)
 rm(tb.paises)
 rm(filtro.edad)
 
-save(share.clean.paises.3.olas,file="tempData/share_def4.RData")
+# save(share.clean.paises.3.olas,file="tempData/share_def4.RData")
